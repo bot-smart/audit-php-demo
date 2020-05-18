9093388ff6e3f40278b8cc23c645d4f1f048db58
licilin <229589561@qq.com>
2020-05-18 22:36:10 +0800

    init

--- auditDemo.php
+++ auditDemo.php
@@ -1,11 +1,8 @@
 <?php
 
-//define("APP_ID", "请输入appId");
-//define("APP_SECRET", "请输入appKey");
-//define("BUSINESS_ID", "请输入businessID");
-define("APP_ID", "cb132453678f49b9a288b606ad4a8a26");
-define("APP_SECRET", "b7530829add540219cca60edfa01dbcf");
-define("BUSINESS_ID", "ecwm3buz33vw");
+define("APP_ID", "请输入appId");
+define("APP_SECRET", "请输入appKey");
+define("BUSINESS_ID", "请输入businessID");
 define("API_URL", "https://api.botsmart.cn/v1/check/send");
 define("API_TIMEOUT", 10);
 require("util.php");
